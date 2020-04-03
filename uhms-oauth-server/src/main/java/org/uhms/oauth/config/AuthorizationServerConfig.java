429210383ccc929c6f30cde85aa75603e1e3b0a8
张子军 <zhangzijun001@outlook.com>
2020-04-03 18:02:33 +0800

    使用redis存储token信息

--- uhms-oauth-server/src/main/java/org/uhms/oauth/config/AuthorizationServerConfig.java
+++ uhms-oauth-server/src/main/java/org/uhms/oauth/config/AuthorizationServerConfig.java
@@ -3,14 +3,19 @@ package org.uhms.oauth.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
 import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 import javax.sql.DataSource;
 
@@ -33,20 +38,57 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 //        return DataSourceBuilder.create().build();
 //    }
 
-    @Bean
-    public JdbcTokenStore jdbcTokenStore(){
-        return new JdbcTokenStore(dataSource);
-    }
+    @Autowired
+    public RedisConnectionFactory redisConnectionFactory;
 
     @Bean
-    public ClientDetailsService jdbcClientDetails(){
+    public TokenStore tokenStore() {
+        RedisTokenStore redis = new RedisTokenStore(redisConnectionFactory);
+        return redis;
+    }
+
+//    @Bean
+//    public JdbcTokenStore jdbcTokenStore(){
+//        return new JdbcTokenStore(dataSource);
+//    }
+
+    @Bean
+    public ClientDetailsService jdbcClientDetails() {
         return new JdbcClientDetailsService(dataSource);
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
-        endpoints.tokenStore(jdbcTokenStore());
+        endpoints.authenticationManager(authenticationManager)
+                .tokenServices(tokenService())
+                .tokenStore(tokenStore());
+//        endpoints.tokenStore(jdbcTokenStore());
+    }
+
+    @Bean
+    public DefaultTokenServices tokenService() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        //配置token存储
+        tokenServices.setTokenStore(tokenStore());
+        //开启支持refresh_token，此处如果之前没有配置，启动服务后再配置重启服务，可能会导致不返回token的问题，解决方式：清除redis对应token存储
+        tokenServices.setSupportRefreshToken(true);
+        //复用refresh_token
+        tokenServices.setReuseRefreshToken(true);
+        //token有效期，设置12小时
+        tokenServices.setAccessTokenValiditySeconds(12 * 60 * 60);
+        //refresh_token有效期，设置一周
+        tokenServices.setRefreshTokenValiditySeconds(7 * 24 * 60 * 60);
+        return tokenServices;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        //允许客户端表单提交
+        security.allowFormAuthenticationForClients()
+                //客户端校验token访问许可
+                .checkTokenAccess("permitAll()")
+                //客户端token调用许可
+                .tokenKeyAccess("permitAll()");
     }
 
     @Override
